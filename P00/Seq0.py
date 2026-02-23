8577e92a92e216997970eb2a04474af2f8560184
salma-06ekh <s.elhamidi.2025@alumnos.urjc.es>
2026-02-23 18:43:46 +0100

    Seq0

--- P00/Seq0.py
+++ P00/Seq0.py
@@ -10,3 +10,46 @@ def seq_read_fasta(filename):
                 continue
             sequence += line.strip()
     return sequence
+
+def seq_len(seq):
+    return len(seq)
+
+def seq_count_base(seq, base):
+    count = 0
+    for i in seq:
+        if base == i:
+            count += 1
+    return count
+
+
+def seq_count(seq):
+    d = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    for base in seq:
+        if base in d:
+            d[base] += 1
+
+    return d
+
+def seq_reverse(seq, n):
+    f = seq[:n]
+    rev = f[::-1]
+    return rev
+
+def seq_complement(seq):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    comp_seq = ""
+    for base in seq:
+        if base in complement:
+            comp_seq += complement[base]
+    return comp_seq
+
+
+def most_frequent_base(sequence):
+    max_count = 0
+    most_frequent = ""
+    for base in sequence:
+        count = sequence.count(base)
+        if count > max_count:
+            max_count = count
+            most_frequent = base
+    return most_frequent
