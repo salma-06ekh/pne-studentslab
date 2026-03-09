c376f3a38b018f08c44b061551c4cbcf053bd56a
salma-06ekh <s.elhamidi.2025@alumnos.urjc.es>
2026-03-09 21:31:37 +0100

    Exercise 3

---
+++ P01/e3.py
@@ -0,0 +1,12 @@
+from Seq1 import Seq
+
+print("-----| Practice 1, Exercise 3 |-----")
+
+
+s1 = Seq()
+s2 = Seq("TATAC")
+s3 = Seq("Invalid sequence")
+
+print("Sequence 1: ", s1)
+print("Sequence 2: ", s2)
+print("Sequence 3: ", s3)
