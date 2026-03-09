4d5f91b0fb0171f3069357edac0fb4568ff9bcbc
salma-06ekh <s.elhamidi.2025@alumnos.urjc.es>
2026-03-09 21:31:47 +0100

    Exercise 4

---
+++ P01/e4.py
@@ -0,0 +1,12 @@
+from Seq1 import Seq
+
+print("-----| Practice 1, Exercise 4 |-----")
+
+
+s1 = Seq()
+s2 = Seq("TATAC")
+s3 = Seq("Invalid sequence")
+
+print(f"Sequence 1: (Length: {len(s1)} {s1}")
+print(f"Sequence 2: (Length: {len(s2)} {s2}")
+print(f"Sequence 3: (Length: {len(s3)} {s3}")
