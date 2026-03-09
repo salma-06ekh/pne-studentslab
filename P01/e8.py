a7a9428d5190d0fd6855fbbe1b14fad04f74b47f
salma-06ekh <s.elhamidi.2025@alumnos.urjc.es>
2026-03-09 21:32:24 +0100

    Exercise 8

---
+++ P01/e8.py
@@ -0,0 +1,23 @@
+from Seq1 import Seq
+
+
+print("-----| Practice 1, Exercise 8 |-----")
+
+s1 = Seq()
+s2 = Seq("TATAC")
+s3 = Seq("Invalid sequence")
+
+print(f"Sequence 1: (Length: {len(s1)}) {s1}")
+print(f"  Bases: {s1.count()}")
+print(f"  Reverse: {s1.reverse()}")
+print(f"  Complement: {s1.complement()}")
+
+print(f"Sequence 2: (Length: {len(s2)}) {s2}")
+print(f"   Bases: {s2.count()}")
+print(f"   Reverse: {s2.reverse()}")
+print(f"   Complement: {s2.complement()}")
+
+print(f"Sequence 3: (Length: {len(s3)}) {s3}")
+print(f"   Bases: {s3.count()}")
+print(f"   Reverse: {s3.reverse()}")
+print(f"   Complement: {s3.complement()}")
