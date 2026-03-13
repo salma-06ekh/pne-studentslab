ffb14fef620a7233dcd3b1975916a89256159ae3
salma-06ekh <s.elhamidi.2025@alumnos.urjc.es>
2026-03-13 14:13:31 +0100

    echo-server2.py

---
+++ S10/echo-server2.py
@@ -0,0 +1,56 @@
+import socket
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1"
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+connections = 0
+
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
+
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+    except KeyboardInterrupt:
+        print("Client stopped")
+        ls.close()
+        exit()
+
+    else:
+        connections += 1
+        print(f"Client connected: {connections}. Client IP, PORT: {client_ip_port}")
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-readable string
+        msg = msg_raw.decode()
+
+        # -- Print the received message
+        print(f"Message received: {msg}")
+        # -- Send a response message to the client
+        response = "ECHO: " + msg
+
+        # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+        # -- Close the data socket
+        cs.close()
